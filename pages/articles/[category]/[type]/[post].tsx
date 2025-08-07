00bf73f40c7fbf6f375e61d6b5efa02341bfa6f6
teruhikookuyama <teruhikookuyama@example.com>
2025-08-07 13:27:20 +0900

    Fix Labubu display: remove product image and consolidate ads into one section

--- pages/articles/[category]/[type]/[post].tsx
+++ pages/articles/[category]/[type]/[post].tsx
@@ -352,52 +352,44 @@ export default function ArticleDetail({ content, frontMatter, category, type, po
       </div>
     </div>
     
-    {/* POP MART Labubu */}
-    <GlobalHotPicksAffiliate 
-      product={{
-        name: "POP MART Labubu Vinyl Toy",
-        image: "https://images.unsplash.com/photo-1558618666-fcd25c85cd64?w=300&h=300&fit=crop&crop=center",
-        amazonUrl: "https://www.amazon.co.jp/dp/B00PBX3L7K",
-        rakutenUrl: "https://item.rakuten.co.jp/popmart/labubu/",
-        officialUrl: "https://global.popmart.com"
-      }}
-    />
-    
-    {/* Labubu 楽天広告 */}
+    {/* POP MART Labubu - 広告ありのため写真付き */}
     <div className="bg-white rounded-xl shadow-md p-6 mb-6">
       <div className="text-center">
-        <a 
-          href="//af.moshimo.com/af/c/click?a_id=5122395&amp;p_id=54&amp;pc_id=54&amp;pl_id=616&amp;url=https%3A%2F%2Fitem.rakuten.co.jp%2Fyinai%2F1000000952%2F&amp;m=http%3A%2F%2Fm.rakuten.co.jp%2Fyinai%2Fi%2F10000351%2F" 
-          rel="nofollow" 
-          referrerPolicy="no-referrer-when-downgrade"
-          className="inline-block"
-        >
+        <h4 className="text-lg font-semibold text-gray-900 mb-4">POP MART Labubu Vinyl Toy</h4>
+        
+        {/* 楽天広告 */}
+        <div className="mb-6">
+          <a 
+            href="//af.moshimo.com/af/c/click?a_id=5122395&amp;p_id=54&amp;pc_id=54&amp;pl_id=616&amp;url=https%3A%2F%2Fitem.rakuten.co.jp%2Fyinai%2F1000000952%2F&amp;m=http%3A%2F%2Fm.rakuten.co.jp%2Fyinai%2Fi%2F10000351%2F" 
+            rel="nofollow" 
+            referrerPolicy="no-referrer-when-downgrade"
+            className="inline-block"
+          >
+            <img 
+              src="//thumbnail.image.rakuten.co.jp/@0_mall/yinai/cabinet/free_1752313526029.jpg?_ex=128x128" 
+              alt="Labubu ぬいぐるみ ペンダント" 
+              style={{ border: 'medium' }} 
+              className="mx-auto"
+            />
+            <br />
+            <span className="text-sm text-gray-700 mt-2 block">
+              【正規品保証・当日発送】 THE MONSTERS Exciting Macaron シリーズ ラブブ- ぬいぐるみ ペンダント【アソートボックス】【ピース】 POP MART ポップマート popmart Labubu rabubu ラブブ キャラクター らぶぶ キーホルダー ラブブ本体 かわいい おもちゃ ギフト プレゼント
+            </span>
+          </a>
           <img 
-            src="//thumbnail.image.rakuten.co.jp/@0_mall/yinai/cabinet/free_1752313526029.jpg?_ex=128x128" 
-            alt="Labubu ぬいぐるみ ペンダント" 
-            style={{ border: 'medium' }} 
-            className="mx-auto"
+            src="//i.moshimo.com/af/i/impression?a_id=5122395&amp;p_id=54&amp;pc_id=54&amp;pl_id=616" 
+            alt="" 
+            loading="lazy" 
+            width="1" 
+            height="1" 
+            style={{ border: '0px' }} 
           />
-          <br />
-          <span className="text-sm text-gray-700 mt-2 block">
-            【正規品保証・当日発送】 THE MONSTERS Exciting Macaron シリーズ ラブブ- ぬいぐるみ ペンダント【アソートボックス】【ピース】 POP MART ポップマート popmart Labubu rabubu ラブブ キャラクター らぶぶ キーホルダー ラブブ本体 かわいい おもちゃ ギフト プレゼント
-          </span>
-        </a>
-        <img 
-          src="//i.moshimo.com/af/i/impression?a_id=5122395&amp;p_id=54&amp;pc_id=54&amp;pl_id=616" 
-          alt="" 
-          loading="lazy" 
-          width="1" 
-          height="1" 
-          style={{ border: '0px' }} 
-        />
-      </div>
-    </div>
-    
-    {/* Labubu Yahoo広告 */}
-    <div className="bg-white rounded-xl shadow-md p-6 mb-6">
-      <div className="text-center">
-        <div data-vc_mylinkbox_id="891949963" />
+        </div>
+        
+        {/* Yahoo広告 */}
+        <div>
+          <div data-vc_mylinkbox_id="891949963" />
+        </div>
       </div>
     </div>
   </section>
