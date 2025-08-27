df640bb3d04c4eb3e19e1b8fe5822873096fd0d6
teruhikookuyama <teruhikookuyama@example.com>
2025-08-27 10:11:12 +0900

    feat(aff): 入口アフィリンクを既定化し未指定時に自動適用＋8/24記事を入口運用へ変更

---
+++ components/aff/aff-config.ts
@@ -0,0 +1,23 @@
+export const AFF_DEFAULT = {
+  Amazon: {
+    // Amazon.co.jp 入口（もしも）
+    href: "//af.moshimo.com/af/c/click?a_id=5122703&p_id=170&pc_id=185&pl_id=4062",
+    img:  "//i.moshimo.com/af/i/impression?a_id=5122703&p_id=170&pc_id=185&pl_id=4062",
+    title: "Amazon.co.jp トップページ",
+    label: "Amazon.co.jp",
+  },
+  Rakuten: {
+    // 楽天市場 入口（もしも）
+    href: "//af.moshimo.com/af/c/click?a_id=5122395&p_id=54&pc_id=54&pl_id=621",
+    img:  "//i.moshimo.com/af/i/impression?a_id=5122395&p_id=54&pc_id=54&pl_id=621",
+    title: "楽天市場 トップページ",
+    label: "楽天市場",
+  },
+  Yahoo: {
+    // Yahoo!ショッピング 入口（ValueCommerce）
+    href: "//ck.jp.ap.valuecommerce.com/servlet/referral?sid=3751180&pid=892011142",
+    img:  "//ad.jp.ap.valuecommerce.com/servlet/gifbanner?sid=3751180&pid=892011142",
+    title: "Yahoo!ショッピング トップページ",
+    label: "Yahoo!ショッピング",
+  },
+} as const; 
