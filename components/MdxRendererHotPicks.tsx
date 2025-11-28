d3d3106fc3bf02158a5f76cd76e6fc9837aad2fe
temasamo <busintemasamo1@gmail.com>
2025-11-28 10:17:16 +0900

    アフィリエイトリンクの表示を修正：ボタン前のテキストを削除し、AffButtonにchildrenを追加

--- components/MdxRendererHotPicks.tsx
+++ components/MdxRendererHotPicks.tsx
@@ -15,6 +15,7 @@ export default function MdxRendererHotPicks({
         p: (props: any) => <p className="mb-4 leading-relaxed" {...props} />,
         li: (props: any) => <li className="text-gray-700 mb-1" {...props} />,
         ul: (props: any) => <ul className="list-disc list-inside mb-4 space-y-2" {...props} />,
+        span: (props: any) => <span {...props} />,
       }}
     />
   );
